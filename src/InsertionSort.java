8dbe4ac7955b126f6bee19c10680a9eb947c1c7e
taegyoo1104 <taegyoo1104@gmail.com>
2021-10-18 00:43:56 +0900

    삽입정렬

---
+++ src/InsertionSort.java
@@ -0,0 +1,20 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 2, 8, 9, 4, 6, 1, 5};
+
+        for(int i = 1; i < arr.length; i++) {
+            int target = arr[i];
+            int j = i - 1;
+
+            while(j>=0 && arr[j] > target) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = target;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
