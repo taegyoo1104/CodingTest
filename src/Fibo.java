74c30ef2a4bad0c2e841d4bce49bfd50425f3e56
taegyoo1104 <taegyoo1104@gmail.com>
2021-10-17 23:45:06 +0900

    Initial commit

---
+++ src/Fibo.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Fibo {
+
+    // 재귀함수 사용
+    public static int fiboRecur(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fiboRecur(n - 1) + fiboRecur(n - 2);
+    }
+
+    // 메모이제이션 사용
+    public static int[] dp;
+    public static int fiboMemo(int n) {
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 3; i <= n; i++ ) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(fiboRecur(number));
+
+        dp = new int[number + 1];
+        System.out.println(fiboMemo(number));
+
+    }
+
+}
