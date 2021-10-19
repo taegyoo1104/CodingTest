cebb1f35bd700dd9f4877819662526836e481a12
taegyoo1104 <taegyoo1104@gmail.com>
2021-10-20 00:40:28 +0900

    버블소트

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -5,7 +5,7 @@ public class BubbleSort {
         int[] arr = {7, 3, 2, 8, 9, 4, 6, 1, 5};
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
