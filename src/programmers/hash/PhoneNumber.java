48c56086317c995fee79d4e44f468c9b4a637629
taegyoo1104 <taegyoo1104@gmail.com>
2021-12-02 17:22:54 +0900

    전화번호 목록

---
+++ src/programmers/hash/PhoneNumber.java
@@ -0,0 +1,27 @@
+package programmers.hash;
+
+import java.util.*;
+
+class PhoneNumber {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], 0);
+        }
+
+        for(int i = 0; i < phone_book.length; i++) {
+            for(int j = 0; j < phone_book[i].length(); j++) {
+                if(map.containsKey(phone_book[i].substring(0,j))) {
+                    return false;
+                }
+            }
+
+        }
+
+
+
+        return answer;
+    }
+}
