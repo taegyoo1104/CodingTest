23620f1a59250a26e834374d665c13eec12206cf
taegyoo1104 <taegyoo1104@gmail.com>
2021-12-02 18:53:03 +0900

    K번째 수

---
+++ src/programmers/sorting/KthNumber.java
@@ -0,0 +1,19 @@
+package programmers.sorting;
+
+import java.util.*;
+
+class KthNumber {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++) {
+            int[] temp = new int[commands[i][1] - commands[i][0] + 1];
+            for(int j = 0; j < temp.length; j++) {
+                temp[j] = array[j+commands[i][0] -1];
+            }
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
